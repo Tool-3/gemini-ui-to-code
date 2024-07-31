522c1d6f441ef32ed7e2bd2e2f301d60763b66fa
Tool-3 <tool.3074@gmail.com>
2024-07-31 10:52:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 import google.generativeai as genai
 
 # Configure the API key directly in the script
-API_KEY = 'YOUR KEY'
+API_KEY = "YOUR_KEY"
 genai.configure(api_key=API_KEY)
 
 # Generation configuration
